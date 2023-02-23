6b7e63d0178cfca14eece88afc3b6a72f41c5479
yamatosuzuko <yamatomatomatomatomato@gmail.com>
2023-02-23 11:21:55 +0900

    Initial Commit

---
+++ firebase_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebase_practice
+//
+//  Created by ysuzuki on 2023/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
